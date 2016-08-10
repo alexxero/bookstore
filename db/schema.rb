d41ee478fb28b579465ab50b6d56ea7fde7e1cc5
alexxero <alexxero@mail.ru>
2016-08-10 12:27:07 +0300

    New column: published_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727180728) do
+ActiveRecord::Schema.define(version: 20160810091927) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "category"
     t.decimal  "price"
     t.integer  "isbn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "published_at"
   end
 
   create_table "searches", force: :cascade do |t|
