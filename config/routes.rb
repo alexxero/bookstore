117eb46aa09839b24a617c5e16ecb736bd4e0953
alexxero <alexxero@mail.ru>
2016-08-10 12:33:23 +0300

    New route: publish_book_path
    resourse books do
    member do
    patch :publish
    end
    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books
+  resources :books do
+    member do
+      patch :publish
+    end
+  end
   resources :searches
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
